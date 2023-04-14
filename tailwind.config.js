895eda39e5ff15b504a82c75288ea2fad84967ca
Edvinas <edvinas.kilciauskas@gmail.com>
2023-04-14 10:25:14 +0300

    create styling

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    colors: {
+      "white": "#FFFFFF",
+      "gray-light": "#d6e2f0",
+      "gray-blue": "#7b879d",
+      "blue": "#1f3251"
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
